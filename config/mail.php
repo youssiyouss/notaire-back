3765e0f85d0c0daf3507123aa5eb658f38a27019
ymecifi.techmind <ymecifi@techmind-innovation.com>
2025-04-14 17:38:12 +0100

    fixing mail error

--- config/mail.php
+++ config/mail.php
@@ -45,7 +45,7 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 120,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
